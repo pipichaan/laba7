f48b3d4b657fd30799e3421e1f8e8c62c85924c4
pipichaan <147584671+pipichaan@users.noreply.github.com>
2023-12-22 01:17:41 +0300

    Add files via upload

---
+++ 7.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите предложение");
+string s = Console.ReadLine();
+string[] words = s.Split(' ');
+string min= words[0];
+foreach (string word in words)
+{
+    if (min.Length > word.Length)
+    {
+        min = word;
+    }
+}
+Console.WriteLine(min );
+
+
+
